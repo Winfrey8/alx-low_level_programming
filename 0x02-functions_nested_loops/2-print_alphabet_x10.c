4d6a4bd3db973b3983e0997cafd8333eb118ebbd
Winfred Njuguna <winfrednju@gmail.com>
2022-09-15 13:00:35 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+88
+*print_alphabet_x10 -function to print alphabets 10 times
+*
+*return: 0
+*/
+void print_alphabet_x10(void);
+{
+	char c, i;
+	for (i = 0; i <= 9, c++)
+	{
+		for (c ='a'; c <= 'z'; c++)
+		{_putchar(c);
+		}
+	}
+clear
+
+}
+
