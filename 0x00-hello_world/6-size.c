dbcc1a2765f53559f4ee036a59e6782643d6c91e
Winfred Njuguna <winfrednju@gmail.com>
2022-09-16 07:51:10 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,26 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - size of various types
+ *
+ * Description: program that prints the size of various types on the computer
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	long longType;
+	long long longlongType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of long: %zu bytes\n", sizeof(longType));
+	printf("Size of long long: %zu bytes\n", sizeof(longlongType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
+}
