037fde60478a6d50c81da3c1abab6b52ead59780
Winfred Njuguna <winfrednju@gmail.com>
2022-09-16 06:45:31 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,14 @@
 
 /**
  * main -print the string in the printf function
+ *
  * prints "with proper grammar, but the outcome is a piece of art,"
+ *
  * return: 0
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
