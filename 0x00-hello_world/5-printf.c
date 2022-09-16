067175653e95d1c87ae73c6c3efc6471260c1c22
Winfred Njuguna <winfrednju@gmail.com>
2022-09-16 06:28:40 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
 /**
- * main -print the string in the put function
+ * main -print the string in the printf function
  * 
- * Description: using the main function
- * this program prints "programming is like bui
+ * this program prints "with proper grammar, but the outcome is a piece of art,"
+ * return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
