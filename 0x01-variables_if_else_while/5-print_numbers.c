97f623725d13fb29961897cabccf0a079661868d
Winfred Njuguna <winfrednju@gmail.com>
2022-09-19 03:18:36 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits of base ten
+ *
+ * Description: using the main function
+ * the program prints single digit numbers
+ * of base ten
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
