eecbf9ce483364cad8dfddee454a090182b53d29
Winfred Njuguna <winfrednju@gmail.com>
2022-09-19 03:08:02 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lower case except q and e
+ *
+ * Description: using the main function
+ * the program prints alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	if (ch = !'e' && ch = !'q')
+	{
+		putchar(ch);
+	}
+	}
+putchar("\n");
+return (0);
+}
+
